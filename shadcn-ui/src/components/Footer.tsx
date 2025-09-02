ad7cefac36e6a86fa63bbed89f83182faaee76b5
Maddibiona Lokesh <2100030312@kluniversity.in>
2025-09-02 16:38:05 +0530

    hello

--- shadcn-ui/src/components/Footer.tsx
+++ shadcn-ui/src/components/Footer.tsx
@@ -22,7 +22,7 @@ export default function LocationSection() {
             width="100%"
             height="100%"
             style={{ border: 0 }}
-            allowFullScreen=""
+            // allowFullScreen=""
             loading="lazy"
           ></iframe>
         </div>
